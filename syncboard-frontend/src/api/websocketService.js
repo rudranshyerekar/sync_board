d9b5f5383dccca081a6db6ed24afc68a491970b2
rudranshyerekar <rudranshyerekar45@gmail>
2026-07-22 19:45:25 +0530

    small_correction_Implement_the_reconnect

--- syncboard-frontend/src/api/websocketService.js
+++ syncboard-frontend/src/api/websocketService.js
@@ -5,6 +5,12 @@ class RealWebSocketService {
   constructor() {
     this.client = null;
     this.subscribers = new Map(); // topic -> Array of callbacks
+    this.hasConnectedOnce = false;
+    this.onReconnectCallback = null;
+  }
+
+  setOnReconnect(callback) {
+    this.onReconnectCallback = callback;
   }
 
   connect(token, onConnect) {
@@ -31,6 +37,14 @@ class RealWebSocketService {
 
     this.client.onConnect = (frame) => {
       console.log("[WebSocket] Connected successfully.");
+      
+      if (this.hasConnectedOnce) {
+        console.log("[WebSocket] Reconnected to server. Resyncing state...");
+        if (this.onReconnectCallback) this.onReconnectCallback();
+      } else {
+        this.hasConnectedOnce = true;
+      }
+
       if (onConnect) onConnect();
       
       // Resubscribe to all active topics on reconnect
